1ae37c9596aa19530c3a873a83bf88ab2e03374c
Kevin Cortacero <twarz.lab@gmail.com>
2023-09-21 15:23:39 +0200

    new primitive system

--- src/kartezio/improc/primitives.py
+++ src/kartezio/improc/primitives.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass, field
 from typing import List, Callable, Sequence
 
+import cv2
+
+from kartezio.model.registry import registry
+from kartezio.model.library import EmptyLibrary
 from kartezio.model.primitive import KPrimitive, KSignature
 from kartezio.model.types import KType, TypeArray
 
@@ -19,6 +23,19 @@ class SignatureTwoImage(SignatureImage):
     input_types: Sequence[KType] = field(init=False, default_factory=lambda: [TypeArray, TypeArray])
 
 
+def f_max(x, args=None):
+    return cv2.max(x[0], x[1])
+
+
+def f_min(x, args=None):
+    return cv2.min(x[0], x[1])
+
+
+p_max = KPrimitive("max", SignatureTwoImage("Max"), f_max)
+p_min = KPrimitive("min", SignatureTwoImage("Min"), f_min)
+# registry.primitives.add("max", replace=True)(p_max)
+
+
 if __name__ == '__main__':
     signature = SignatureImage("test", [TypeArray], n_parameters=0)
     signature_1 = SignatureOneImage("test_1", n_parameters=2)
@@ -26,3 +43,8 @@ if __name__ == '__main__':
     print(signature)
     print(signature_1)
     print(signature_2)
+
+    library_test = EmptyLibrary(TypeArray)
+    library_test.add_primitive(p_max)
+    library_test.add_primitive(p_min)
+    library_test.display()
