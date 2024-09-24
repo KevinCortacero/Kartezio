25eac7e835c02d48ec3730adb6cdd74ef2c31038
Kevin Cortacero <kevin.cortacero@protonmail.com>
2024-09-24 15:33:07 +0200

    merged preprocessing

--- src/kartezio/preprocessing.py
+++ src/kartezio/preprocessing.py
@@ -23,136 +23,131 @@ class PyrMeanShift(Preprocessing):
     def preprocess(self, x, args=None):
         new_x = []
         for i in range(len(x)):
-            original_image = cv2.merge(x[i])
+            original_image = cv2.merge(x[i][:3])
             filtered = cv2.pyrMeanShiftFiltering(original_image, sp=self.sp, sr=self.sr)
-            cv2.imwrite(f"pyr_{i}.png", filtered)
             new_x.append(image_split(filtered))
         return new_x
 
 
 @register(Preprocessing, "pyr_scale")
 class PyrScale(Preprocessing):
-    def __init__(self, level, scale: str):
+    def __init__(self, level, scale: str, preserve_values=False):
         super().__init__()
         self.level = level
         self.scale = scale
+        self.preserve_values = preserve_values
 
     def preprocess(self, x, args=None):
-        if self.level == 0:
-            return x
         new_x = []
         for i in range(len(x)):
             new_xi = []
             for xij in x[i]:
-                for l in range(self.level):
-                    if self.scale == "down":
-                        xij = cv2.pyrDown(xij.astype(np.uint8))
-                    elif self.scale == "up":
-                        xij = cv2.pyrUp(xij.astype(np.uint8))
+                dsize = (xij.shape[1], xij.shape[0])
+                if dsize[0] % 2 != 0:
+                    dsize = (dsize[0] + 1, dsize[1])
+                if dsize[1] % 2 != 0:
+                    dsize = (dsize[0], dsize[1] + 1)
+                if self.level == 0:
+                    xij = cv2.resize(xij, dsize, interpolation=cv2.INTER_NEAREST)
+                else:
+                    for l in range(self.level):
+                        if self.preserve_values:
+                            if self.scale == "down":
+                                dsize = (dsize[0] // 2, dsize[1] // 2)
+                                xij = cv2.resize(xij, dsize, interpolation=cv2.INTER_NEAREST)
+                            elif self.scale == "up":
+                                dsize = (dsize[0] * 2, dsize[1] * 2)
+                                xij = cv2.resize(xij, dsize, interpolation=cv2.INTER_NEAREST)
+                        else:
+                            if self.scale == "down":
+                                xij = cv2.pyrDown(xij.astype(np.uint8))
+                            elif self.scale == "up":
+                                xij = cv2.pyrUp(xij.astype(np.uint8))
                 new_xi.append(xij)
             new_x.append(new_xi)
         return new_x
 
 
+@register(Preprocessing, "to_color_space")
+class ToColorSpace(Preprocessing):
+    def __init__(self, color_space):
+        super().__init__()
+        self.color_space = color_space
 
-@register(Preprocessing, "to_hsv")
-class TransformToHSV(Preprocessing):
-    def preprocess(self, x, args=None):
+    def preprocess(self, x):
         new_x = []
         for i in range(len(x)):
-            original_image = cv2.merge(x[i])
-            transformed = rgb2hsv(original_image)
-            new_x.append(image_split(transformed))
+            # assuming that the image (3 first elements) is in RGB
+            original_image = cv2.merge(x[i][:3])
+            if self.color_space == "hed":
+                transformed = image_split(rgb2hed(original_image))
+            elif self.color_space == "hsv":
+                transformed = image_split(rgb2hsv(original_image))
+            elif self.color_space == "bgr":
+                transformed = image_split(rgb2bgr(original_image))
+            elif self.color_space == "lab":
+                transformed = image_split(cv2.cvtColor(original_image, cv2.COLOR_RGB2LAB))
+            elif self.color_space == "gray":
+                transformed = cv2.cvtColor(original_image, cv2.COLOR_RGB2GRAY)
+            if len(x[i]) > 3:
+                new_x.append(transformed + list(x[i][3:]))
+            else:
+                new_x.append(transformed)
         return new_x
-    
 
-@register(Preprocessing, "clahe_value")
-class ClaheValue(Preprocessing):
+
+@register(Preprocessing, "add_color_space")
+class AddColorSpace(Preprocessing):
+    def __init__(self, color_space):
+        super().__init__()
+        self.color_space = color_space
+
+    def preprocess(self, x):
+        new_x = []
+        for i in range(len(x)):
+            # assuming that the image (3 first elements) is in RGB
+            original_image = cv2.merge(x[i][:3])
+            if self.color_space == "hed":
+                transformed = image_split(rgb2hed(original_image))
+            elif self.color_space == "hsv":
+                transformed = image_split(rgb2hsv(original_image))
+            elif self.color_space == "bgr":
+                transformed = image_split(rgb2bgr(original_image))
+            elif self.color_space == "lab":
+                transformed = image_split(cv2.cvtColor(original_image, cv2.COLOR_RGB2LAB))
+            elif self.color_space == "gray":
+                transformed = cv2.cvtColor(original_image, cv2.COLOR_RGB2GRAY)
+            new_x.append(list(x[i]) + transformed)
+        return new_x
+
+
+@register(Preprocessing, "clahe")
+class ApplyClahe(Preprocessing):
     def __init__(self, clip_limit=2.0, tile_grid_size=(8, 8)):
         super().__init__()
         self.clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=tile_grid_size)
 
-    def preprocess(self, x, args=None):
+    def preprocess(self, x):
         new_x = []
         for i in range(len(x)):
             original_image = cv2.merge(x[i])
-            transformed = rgb2hsv(original_image)
-            transformed[:, :, 2] = self.clahe.apply(transformed[:, :, 2])
-            new_x.append(image_split(hsv2rgb(transformed)))
-        return new_x
-
-
-@register(Preprocessing, "to_hed")
-class TransformToHED(Preprocessing):
-    def preprocess(self, x, args=None):
-        new_x = []
-        for i in range(len(x)):
-            original_image = cv2.merge(x[i])
-            transformed = rgb2hed(original_image)
-            new_x.append(image_split(transformed))
-        return new_x
-    
-
-@register(Preprocessing, "to_gray")
-class ToGray(Preprocessing):
-    def preprocess(self, x, args=None):
-        new_x = []
-        for i in range(len(x)):
-            original_image = cv2.merge(x[i])
-            transformed = cv2.cvtColor(original_image, cv2.COLOR_RGB2GRAY)
-            new_x.append([transformed])
-        return new_x
-    
-@register(Preprocessing, "append_hed")
-class AppendHED(Preprocessing):
-    def preprocess(self, x, args=None):
-        new_x = []
-        for i in range(len(x)):
-            original_image = cv2.merge(x[i][:3])
-            transformed = rgb2hed(original_image)
-            new_x.append(x[i] + image_split(transformed))
-        return new_x
-
-
-@register(Preprocessing, "append_gray")
-class AppendGray(Preprocessing):
-    def preprocess(self, x, args=None):
-        new_x = []
-        for i in range(len(x)):
-            original_image = cv2.merge(x[i][:3])
-            transformed = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-            new_x.append(x[i] + [transformed])
-        return new_x
-    
-
-@register(Preprocessing, "append_hsv")
-class AppendHSV(Preprocessing):
-    def preprocess(self, x, args=None):
-        new_x = []
-        for i in range(len(x)):
-            original_image = cv2.merge(x[i][:3])
-            transformed = rgb2hsv(original_image)
-            new_x.append(x[i] + image_split(transformed))
+            transformed = transformed = cv2.cvtColor(original_image, cv2.COLOR_RGB2GRAY)
+            transformed = self.clahe.apply(transformed)
+            new_x.append(image_split(cv2.cvtColor(transformed, cv2.COLOR_GRAY2RGB)))
         return new_x
 
 
 @register(Preprocessing, "select_channels")
 class SelectChannels(Preprocessing):
-    def __init__(self, channels, scalars=None):
+    def __init__(self, channels):
         super().__init__()
         self.channels = channels
-        self.scalars = scalars
 
     def preprocess(self, x):
         new_x = []
         for i in range(len(x)):
             one_item = [x[i][channel] for channel in self.channels]
-            if self.scalars:
-                # labels = [np.zeros_like(channel, dtype=np.int16) for channel in self.channels]
-                # new_x.append([one_item, self.scalars, labels])
-                new_x.append([one_item, self.scalars])
-            else:
-                new_x.append(one_item)
+            new_x.append(one_item)
         return new_x
 
 
