61d8748f8884363c900a6103b7588273b71e2083
Kévin Cortacero <kevin.cortacero@inserm.fr>
2025-01-12 17:04:00 +0100

    discrete version of decays

--- src/kartezio/mutation/decay.py
+++ src/kartezio/mutation/decay.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Dict
 
 import numpy as np
@@ -10,9 +10,10 @@ from kartezio.mutation.base import Mutation
 
 @fundamental()
 class MutationDecay(UpdatableComponent, ABC):
-    def __init__(self):
+    def __init__(self, discrete_bins: int = None):
         super().__init__()
         self._mutation = None
+        self.discrete_bins = discrete_bins
 
     def set_mutation(self, mutation: Mutation):
         self._mutation = mutation
@@ -20,6 +21,16 @@ class MutationDecay(UpdatableComponent, ABC):
     def update(self, event: Event):
         if event.name == Event.Events.END_STEP:
             self._mutation.node_rate = self.stored[event.iteration]
+
+    def _discretize(self, values):
+        if self.discrete_bins:
+            bin_indices = np.linspace(
+                0, len(values) - 1, self.discrete_bins, dtype=int
+            )
+            bin_size = len(values) // self.discrete_bins
+            for i, index in enumerate(bin_indices):
+                values[i * bin_size : (i + 1) * bin_size] = values[index]
+        return values
 
 
 @register(MutationDecay)
@@ -42,13 +53,14 @@ class LinearDecay(MutationDecay):
     def __from_dict__(cls, dict_infos: Dict) -> "LinearDecay":
         pass
 
-    def __init__(self, start: float, end: float):
-        super().__init__()
+    def __init__(self, start: float, end: float, discrete_bins: int = None):
+        super().__init__(discrete_bins)
         self.start = start
         self.end = end
 
     def _precompute(self):
-        return np.linspace(self.start, self.end, self.n_iterations)
+        values = np.linspace(self.start, self.end, self.n_iterations)
+        return self._discretize(values)
 
 
 @register(MutationDecay)
@@ -57,15 +69,19 @@ class DegreeDecay(MutationDecay):
     def __from_dict__(cls, dict_infos: Dict) -> "DegreeDecay":
         pass
 
-    def __init__(self, degree: int, start: float, end: float):
-        super().__init__()
+    def __init__(
+        self, degree: int, start: float, end: float, discrete_bins: int = None
+    ):
+        super().__init__(discrete_bins)
         self.degree = degree
         self.start = start
         self.end = end
 
     def _precompute(self):
         x = np.linspace(0, 1, self.n_iterations)
-        return (self.end - self.start) * np.power(x, self.degree) + self.start
+        return self._discretize(
+            (self.end - self.start) * np.power(x, self.degree) + self.start
+        )
 
 
 @register(MutationDecay)
@@ -74,12 +90,16 @@ class InvDegreeDecay(MutationDecay):
     def __from_dict__(cls, dict_infos: Dict) -> "InvDegreeDecay":
         pass
 
-    def __init__(self, degree: int, start: float, end: float):
-        super().__init__()
+    def __init__(
+        self, degree: int, start: float, end: float, discrete_bins: int = None
+    ):
+        super().__init__(discrete_bins)
         self.degree = degree
         self.start = start
         self.end = end
 
     def _precompute(self):
         x = np.linspace(1, 0, self.n_iterations)
-        return -(self.end - self.start) * np.power(x, self.degree) + self.end
+        return self._discretize(
+            -(self.end - self.start) * np.power(x, self.degree) + self.end
+        )
